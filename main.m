c466db58afa3cd1c385bc15eda63f1abca4a63c8
Toumey <etoumey>
2017-04-13 15:09:24 -0400

    Initial commit. Spent a lot of time on that header.

---
+++ main.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%% AAE 339 Project %%%%%%%%%%%%%%%%%%%%%
+%  Description: Main script for orbital calculations      %
+%  Team 7:   Henry Heim                                   %
+%            Logan Kirsch                                 %
+%            Chris Prak                                   %
+%            Glen Shumaker                                %  
+%            Eliot Toumey                                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc; clear; close all;
+
+% Constants 
+payloadMass = 0;
