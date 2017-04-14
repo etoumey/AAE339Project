795d1c65786118133014076e5b94ecdec700111f
Heim <hheim>
2017-04-14 11:24:49 -0400

    Add comments to the inclination function

--- deltaVInclination.m
+++ deltaVInclination.m
@@ -3,10 +3,14 @@ function [ deltaV ] = deltaVInclination( alt, angle )
 % given altitude.
 %   alt: Orbit altitude (km)
 %   angle: Desired change in inclination (degrees)
-
+    
+    % Constant efinitions
     G = 6.67408e-11; % m^3 kg^-1 s^-2
     M = 5.9721986e24; % kg
-
+    
+    % Calculate orbital velocity at given altitude
     velocity = sqrt(G * M / (alt * 1000));
+    
+    % Calculate required delta v to change inclination
     deltaV = 2 * velocity * sind(angle / 2);
 end
