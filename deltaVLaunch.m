ffa39304b93a53d6efbfcd102431d6999d03109b
Toumey <etoumey>
2017-04-14 12:07:31 -0400

    Done for now, just have approximate D&G losses

--- deltaVLaunch.m
+++ deltaVLaunch.m
@@ -1,9 +1,14 @@
-function [ deltaV ] = deltaVLaunch( ro, rf, lat )
+function [ deltaV ] = deltaVLaunch( ro, rf, lat, azimuth )
 %UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-  mu = 3.98600441E14; %m^3 s^-2
-  w = 2*pi/(23.95*60*60); %Period of earth in rad/sec
+%   Azimuth in launch trajectory in degrees. 90 is due east.
 
-deltaV = sqrt(mu/(rf)) - ro*cosd(lat)*w; %free DeltaV from earths rotation
+mu = 3.98600441E14; %m^3 s^-2
+w = 2*pi/(23.95*60*60); %Period of earth in rad/sec
+
+deltaVGrav = 1500;  % [m/s]
+deltaVDrag = 150; % [m/s]
+deltaVSteer = 200; % [m/s]
+
+deltaV = sqrt(mu/(rf)) - ro*cosd(lat)*w*sind(azimuth) + deltaVGrav + deltaVDrag + deltaVSteer; %free DeltaV from earths rotation
 
 end
