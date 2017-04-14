63f0851f353e54ea4ddd38a527fa61b0a84763bf
Toumey <etoumey>
2017-04-14 11:50:06 -0400

    Launch deltaV figured out... Need to account for D

--- deltaVLaunch.m
+++ deltaVLaunch.m
@@ -1,9 +1,9 @@
-function [ deltaV ] = deltaVLaunch( ro, rf )
+function [ deltaV ] = deltaVLaunch( ro, rf, lat )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
   mu = 3.98600441E14; %m^3 s^-2
-  
-  deltaV = sqrt(mu/ro)*(sqrt(2*rf/(ro+rf))-1);
+  w = 2*pi/(23.95*60*60); %Period of earth in rad/sec
 
+deltaV = sqrt(mu/(rf)) - ro*cosd(lat)*w; %free DeltaV from earths rotation
 
 end
