e8aa9b092f58ff0e820be3acd379eaed9eef71be
Your Name <etoumey@purdue.edu>
2017-04-17 13:07:27 -0400

    Got some stuff in main. Literally spent 7 minutes doing this. Will work more later

--- main.m
+++ main.m
@@ -13,12 +13,13 @@ payloadMass = 3000; % kg
 lat = 28.4740;
 
 % Orbit charicteristics
+altOriginal = 6.3710088E6; %earth's radius in meters
 altLEO = 100; % km, low earth orbit altitude
 altGEO = 35786; % km, geostationary orbit altitude
 incLEO = lat; % low earth orbit inclination
 incGEO = 0; % geostationary orbit inclinaton
 
-% dVLaunch = deltaVLaunch( ro, rf, lat, azimuth );
-% dVTransfer = deltaVTransfer( r1, r2 );
+dVLaunch = deltaVLaunch( altOriginal, altOriginal+altLEO*1000, lat, 90 );
+dVTransfer = deltaVTransfer( altLEO+altOriginal/1000, altGEO+altOriginal/1000 );
 % dVCircularize = deltaVCircularize( r1, r2 );
 % dVInclination = deltaVInclination( r, angle );
