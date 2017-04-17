480eb8557ed09c5a7851369883b76467ffa8ad8e
Toumey <etoumey@purdue.lcl>
2017-04-17 18:00:35 -0400

    Functionalized Glen's script

--- staging.m
+++ staging.m
@@ -1,15 +1,15 @@
 % Team 7
 % AAE 339 Project Staging
-clear
-clc
+function [ dv1, dv2 ] = staging( delta_v, delta_v3 )
+
 
 g0 = 9.81;
 Msatellite = 7000;
 Isp_sat = 1660;
 Isp2 = 465.5;
 Isp1 = 362;
-delta_v = 12000;
-delta_v3 = 1000;
+%delta_v = 12000;
+%delta_v3 = 1000;
 
 fs2 = 9; % mass of fuel : inert mass (Mf/(Mf+Mi)=.9)
 fs1 = 9; % mass of fuel : inert mass (Mf/(Mf+Mi)=.9)
@@ -37,18 +37,19 @@ Mfuel = Mfuel(Minert>0);
 Minert= Minert(Minert>0);
 
 [Mf,I] = min(Mfuel);
-Mf
-Mi = Minert(I)
+Mf;
+Mi = Minert(I);
 
-delta_v2 = dv2(I)
+delta_v2 = dv2(I);
 
     Ms2 = M03*(exp(delta_v2/(g0*Isp2))-1)/(fs2-((exp(delta_v2/(g0*Isp2))-1)));
     Mfuel_2 = Ms2*fs2;
     M02 = 1.05*(M03+Mfuel_2+Ms2);
-    delta_v1 = delta_v - delta_v2 - delta_v3
+    delta_v1 = delta_v - delta_v2 - delta_v3;
     Ms1 = M02*(exp(delta_v1/(g0*Isp1))-1)/(fs1-((exp(delta_v1/(g0*Isp1))-1)));
     Mfuel_1 = Ms1*fs1;
 
 dv2 = g0*Isp2*log((M03+Ms2+Mfuel_2)/(M03+Ms2));
 dv1 = g0*Isp1*log((M02+Ms1+Mfuel_1)/(M02+Ms1));
 
+end
